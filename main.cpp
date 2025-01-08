75cf066e22d285d927ddc63157b83e260266855c
LittleFox2024 <btdefores@outlook.com>
2025-01-08 20:40:41 +0000

    Updates.

--- main.cpp
+++ main.cpp
@@ -3,4 +3,4 @@ using namespace std;
 
 //All other parts will be added as modules
 //program.cpp should always be last!
-#include program.cpp
+#include "program.cpp"
---
+++ program.cpp
@@ -0,0 +1,11 @@
+//Create vars
+
+char input[1024] = "";
+
+
+int main() {
+    cout << "Welcome to Code 871!\n";
+    cin >> input;
+    cout << input << "\n";
+    return 0;
+}
