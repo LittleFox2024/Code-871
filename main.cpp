1f4b5112414fbc3b0aebb4919435700334e9f8ec
LittleFox2024 <177757947+LittleFox2024@users.noreply.github.com>
2025-03-28 13:11:13 -0400

    Fixed some issue.
    Added loop to main so it loops
    Fixed it so that typing "System Call" and nothing else doesn't put it in an infinite loop.

--- main.cpp
+++ main.cpp
@@ -26,14 +26,21 @@ int main(){
 
     //Welcome
     cout << "Welcome to the Underworld!" << endl;
+    while(true){
+        string inputCommand = "";
+        string exitCommand = "exit";
+        string exactMatch = "System Call:";
+        cout << ">";
+        getline(cin, inputCommand);
     
-    string inputCommand = "";
-    string exitCommand = "exit";
-    cout << ">";
-    getline(cin, inputCommand);
-    
-    if (!(compareStrings(inputCommand, exitCommand))){
-        executeCommand(inputCommand);
+        if (!(compareStrings(inputCommand, exitCommand))){
+            if (!(compareStrings(inputCommand, exactMatch))){
+                executeCommand(inputCommand);
+            }
+        }
+        else{
+            break;
+        }
     }
     return 0;
 }
@@ -46,8 +53,8 @@ int executeCommand(string& command){
 //Contains code to execute commands
     //Create string for comparison
     string sysCallString;
-    string SYSCALL = "System Call";
-    sysCallString = command.substr(0, 11);
+    string SYSCALL = "System Call:";
+    sysCallString = command.substr(0, 12);
     if (compareStrings(sysCallString, SYSCALL)){
         cout << "System Call!!!" << endl;
         executeSysCall(command);
